738b78d24d5a9dcbd62c5f493d1ab80e5a5ec522
iFarbod <ifarbod@outlook.com>
2023-01-07 23:04:19 +0330

    Add project files

---
+++ app/build.gradle.kts
@@ -0,0 +1,107 @@
+import com.android.build.api.variant.FilterConfiguration.FilterType
+import java.io.FileInputStream
+import java.util.Properties
+
+val keystorePropertiesFile = rootProject.file("keystore.properties")
+val useKeystoreProperties = keystorePropertiesFile.canRead()
+val keystoreProperties = Properties()
+if (useKeystoreProperties)
+{
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.ifarbod.myapplication"
+    compileSdk = 33
+    buildToolsVersion = "33.0.1"
+    ndkVersion = "25.1.8937393"
+
+    defaultConfig {
+        applicationId = "com.ifarbod.myapplication"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    signingConfigs {
+        named("debug") {
+            storeFile = file("${System.getProperty("user.home")}/.android/debug.keystore")
+            storePassword = "android"
+            keyAlias = "androiddebugkey"
+            keyPassword = "android"
+            enableV1Signing = true
+            enableV2Signing = true
+            enableV3Signing = true
+            enableV4Signing = true
+        }
+    }
+
+    buildTypes {
+        named("debug") {
+            applicationIdSuffix = ".debug"
+            versionNameSuffix = "-dev"
+
+            isMinifyEnabled = false
+            isShrinkResources = false
+        }
+
+        named("release") {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+            signingConfig = signingConfigs.getByName(if (useKeystoreProperties) "release" else "debug")
+        }
+    }
+    
+    lint {
+        disable += "MissingTranslation"
+    }
+    
+    val javaVersion = JavaVersion.VERSION_11
+    compileOptions {
+        sourceCompatibility = javaVersion
+        targetCompatibility = javaVersion
+    }
+    kotlinOptions {
+        jvmTarget = javaVersion.toString()
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+val abiCodes = mapOf("armeabi-v7a" to 1, "arm64-v8a" to 2, "x86" to 3, "x86_64" to 4)
+
+androidComponents {
+    onVariants { variant ->
+        variant.outputs.forEach { output ->
+            val name = output.filters.find { it.filterType == FilterType.ABI }?.identifier
+            val baseAbiCode = abiCodes[name]
+            if (baseAbiCode != null)
+            {
+                //output.versionCode.set(baseAbiCode * 1_000 + (output.versionCode.orNull ?: 0))
+                output.versionCode.set(baseAbiCode + (output.versionCode.orNull ?: 0).times(1_000))
+            }
+        }
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.8.0")
+    implementation("androidx.appcompat:appcompat:1.4.1")
+    implementation("com.google.android.material:material:1.5.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.2")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.2")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
+}
